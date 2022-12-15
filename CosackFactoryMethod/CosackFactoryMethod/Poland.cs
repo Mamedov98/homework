43e5c94eb13cacbcb26886b638776008cb0fa461
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2022-12-15 17:51:12 +0400

    Add files via upload

---
+++ CosackFactoryMethod/CosackFactoryMethod/Poland.cs
@@ -0,0 +1,31 @@
+﻿using CosackFactoryMethod.Classes;
+using CosackFactoryMethod.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosackFactoryMethod
+{
+    public class Poland : ICountry 
+    {
+        public Army? ArmyCount { get; set; }
+        public Cavalry? CavalryCount { get; set; }
+
+        public Artillery? ArtilleryCount { get; set; }
+
+        public void Characteristics()
+        {
+            Console.WriteLine("Poland");
+            ArmyCount.HP = "200 HP";
+            ArmyCount.Stamina = "400%";
+            ArmyCount.ArmySize = 8000;
+            CavalryCount._Cavalry = 1500;
+            CavalryCount.CavalrySpeed = "43 km/h";
+            ArtilleryCount._Artillery = 40;
+            ArtilleryCount.RateOfFire = "15 shots per minute";
+            Console.WriteLine("Have 18th century");
+        }
+    }
+}
--- CosackFactoryMethod/CosackFactoryMethod/Program.cs
+++ CosackFactoryMethod/CosackFactoryMethod/Program.cs
@@ -3,57 +3,66 @@ using CosackFactoryMethod;
 using CosackFactoryMethod.Classes;
 
 
-int Choice =5 ;
+int Choice = 5;
 while (Choice != 0)
 {
     if (Choice != 0)
     {
-        Console.WriteLine("\n1: Turkiye \n2: Russia \n3: Ukraine \n4: England");
-        
-        Choice = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("\n1: Turkiye \n2: Russia \n3: Ukraine \n4: England \n5: Poland");
+
+        Choice = Convert.ToInt32(Console.ReadLine());
         CountryFactory Creator = new();
         Army Army = new Army();
         Cavalry Cavalry = new Cavalry();
-        Artillery Artillery = new Artillery();   
+        Artillery Artillery = new Artillery();
         switch (Choice)
-        { 
+        {
             case 1:
                 {
 
-                    var Country = Creator.CountryProduction<Turkey>(Army,Cavalry,Artillery);   
-                    Country.Characteristics(); 
-                    Country.Print(); 
-                    Choice = Convert.ToInt32(Console.ReadLine());
-                    break;
-                } 
-            case 2:
-                {
-
-                    var Country = Creator.CountryProduction<Russian>(Army,Cavalry,Artillery);   
-                    Country.Characteristics(); 
-                    Country.Print();             
-                    Choice = Convert.ToInt32(Console.ReadLine());
-                    break;
-                } 
-            case 3:
-                {
-
-                    var Country = Creator.CountryProduction<Ukraine>(Army,Cavalry,Artillery);   
-                    Country.Characteristics(); 
-                    Country.Print();   
-                    Choice = Convert.ToInt32(Console.ReadLine());
-                    break;
-                } 
-            case 4:
-                {
-
-                    var Country = Creator.CountryProduction<England>(Army,Cavalry,Artillery);   
-                    Country.Characteristics();  
-                    Country.Print();   
+                    var Country = Creator.CountryProduction<Turkey>(Army, Cavalry, Artillery);
+                    Country.Characteristics();
+                    Country.Print();
                     Choice = Convert.ToInt32(Console.ReadLine());
                     break;
                 }
-           
+            case 2:
+                {
+
+                    var Country = Creator.CountryProduction<Russian>(Army, Cavalry, Artillery);
+                    Country.Characteristics();
+                    Country.Print();
+                    Choice = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+            case 3:
+                {
+
+                    var Country = Creator.CountryProduction<Ukraine>(Army, Cavalry, Artillery);
+                    Country.Characteristics();
+                    Country.Print();
+                    Choice = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+            case 4:
+                {
+
+                    var Country = Creator.CountryProduction<England>(Army, Cavalry, Artillery);
+                    Country.Characteristics();
+                    Country.Print();
+                    Choice = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+            case 5:
+                {
+
+                    var Country = Creator.CountryProduction<Poland>(Army, Cavalry, Artillery);
+                    Country.Characteristics();
+                    Country.Print();
+                    Choice = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+
 
         }
     }
